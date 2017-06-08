25a2e726bdd342dde26a1a9b1679031dc031881d
Warren Chan <warrenchan@warren-2.local>
2017-06-08 12:04:28 -0700

    players centered

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>
+        <div className="playerContain">
           {this.state.players.map(player => (
             <div className="player">
               <p>{player.firstname}</p>
