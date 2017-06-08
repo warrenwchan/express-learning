b8889ccf3aa13e1af252ecf6322daf99e51aa848
Warren Chan <warrenchan@warren-2.local>
2017-06-08 11:36:27 -0700

    got data to connect. getting data to display on screen

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,17 +5,27 @@ import './App.css';
 class App extends Component {
 
   state = {
-    data: {}
+    players: {}
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/playerdata')
+    fetch('http://localhost:3000/players')
     .then(response => response.json())
-    .then(data => this.setState({data}))
+    .then(players => this.setState({players}))
     .catch(err => console.log)
   }
 
+  mapPlayers() {
+    let players = this.state.players
+
+    for( let i = 0; i < players.length; i++ ) {
+      return <p>{players[i].firstname}</p>
+    }
+  }
+
   render() {
+    console.log(this.state.players)
+    console.log(this.mapPlayers())
     return (
       <div className="App">
         <div className="App-header">
@@ -25,6 +35,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          {this.mapPlayers()}
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express()
+const pgp = require('pg-promise')({
+    // Initialization Options
+});
+const cors = require('cors')
 
+var cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'newtest',
+    user: 'warrenchan',
+    password: 'keys'
+};
 
+var db = pgp(cn);
+
+app.use(express.static(__dirname + '/public/build'));
+
+app.get('/players', cors(), (req, res) => {
+    db.any('SELECT * FROM players')
+    .then(function(players) {
+        res.json(players)
+    })
+    .catch(function(error){
+        console.log('Error', error)
+        res.status(500).end();
+    })
+})
 
 app.listen(3000, function (err) {
     err && console.log(err);
